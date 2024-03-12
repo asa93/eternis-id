f653a138973f70add78306121145d908e687337a
asa93 <asa93@live.fr>
2024-03-12 11:52:13 +0100

    add credentials to non commited file

--- examples/example-backend-2/index.ts
+++ examples/example-backend-2/index.ts
@@ -1,12 +1,8 @@
-// import cors from 'cors'
-// import dotenv from 'dotenv'
-// import express, { Express, Request, Response } from 'express'
-
 import { Reclaim } from '@reclaimprotocol/js-sdk'
 import cors from 'cors'
-//import crypto from 'crypto'
 import dotenv from 'dotenv'
 import express, { Express, Request, Response } from 'express'
+import { credentials } from './reclaim-credentials'
 
 dotenv.config()
 const app: Express = express()
@@ -24,28 +20,26 @@ app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
 })
 
+
+const { appId, appSecret, providerId } = credentials[0]
+
 app.get('/request', async (_, res: Response) => {
-    const APP_ID = '0xaDC7C420ef0259972d44929dBCA3De1fbd7ef70D'
-
-    const reclaimClient = new Reclaim.ProofRequest(
-        APP_ID //TODO: replace with your applicationI
-    )
-
-    const providerIds = [
-        '5f83ab14-9656-4552-9fbc-482e07022766', // Amazon 2023 Order Count
-    ]
-
-    await reclaimClient.buildProofRequest(providerIds[0])
-
-    const APP_SECRET = '0xe4176ead9c383e0ee3cda8679ea66e7e76eb874a5d962fbd990a1d736ca50147'
-
-    reclaimClient.setSignature(
-        await reclaimClient.generateSignature(
-            APP_SECRET //TODO : replace with your APP_SECRET
+    try {
+        const reclaimClient = new Reclaim.ProofRequest(
+            appId //TODO: replace with your applicationI
         )
-    )
+        await reclaimClient.buildProofRequest(providerId)
 
-    const { requestUrl, statusUrl } = await reclaimClient.createVerificationRequest()
-    res.send({ reclaimUrl: requestUrl, statusUrl })
+        reclaimClient.setSignature(
+            await reclaimClient.generateSignature(
+                appSecret //TODO : replace with your APP_SECRET
+            )
+        )
+
+        const { requestUrl, statusUrl } = await reclaimClient.createVerificationRequest()
+        res.send({ reclaimUrl: requestUrl, statusUrl })
+    } catch (e: any) {
+        res.status(400).send(e.toString())
+    }
 
 })
