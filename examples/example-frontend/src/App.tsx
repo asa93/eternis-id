84618ccb978038c81925f022a39bb28ae1492aab
asa93 <asa93@live.fr>
2024-03-12 21:27:26 +0100

    front: add wagmi

--- examples/example-frontend/src/App.tsx
+++ examples/example-frontend/src/App.tsx
@@ -5,6 +5,14 @@ import QRCode from "react-qr-code";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import CircularProgress from "@mui/material/CircularProgress";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider, useAccount } from "wagmi";
+import { configz } from "./config";
+import { Account } from "./account";
+import { WalletOptions } from "./wallet-options";
+
+const queryClient = new QueryClient();
+
 const backendBaseUrl = process.env.REACT_APP_BACKEND_BASE_URL;
 
 type Proof = {
@@ -19,6 +27,12 @@ type Proof = {
     context: String;
   };
 };
+
+function ConnectWallet() {
+  const { isConnected } = useAccount();
+  if (isConnected) return <Account />;
+  return <WalletOptions />;
+}
 
 function App() {
   const [callbackId, setCallbackId] = React.useState<string | null>(null);
@@ -68,76 +82,82 @@ function App() {
   console.log("proof2", proof);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {proof && (
-          <span>
-            {" "}
-            ✅ Proof submitted <br />{" "}
-            {proof?.extractedParameterValues?.CLAIM_DATA}{" "}
-          </span>
-        )}
-
-        {!reclaimUrl && (
-          <div>
-            <h2>DEMO</h2>
-            <h3>Prove that you are a real person</h3>
-            <p>[This demo uses HttpsProvider and CustomProvider]</p>
-
-            <div className="actions">
-              <button
-                className="button"
-                disabled={!!reclaimUrl}
-                onClick={proveIt}
-              >
-                Prove it!
-              </button>
-            </div>
-          </div>
-        )}
-        {reclaimUrl && !proof && (
-          <div className="links">
-            {proofStatus && (
+    <WagmiProvider config={configz}>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <ConnectWallet />
+          <header className="App-header">
+            {proof && (
               <span>
-                Status {proofStatus}... <CircularProgress />
+                {" "}
+                ✅ Proof submitted <br />{" "}
+                {proof?.extractedParameterValues?.CLAIM_DATA} <hr />
               </span>
             )}
-            <div>
-              If you don't have our app installed, check the installation steps{" "}
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className="App-link"
-                href="https://questbook.gitbook.io/reclaim-protocol/installing-reclaim-wallet"
-              >
-                here
-              </a>
-            </div>
-          </div>
-        )}
-        {reclaimUrl ? (
-          <>
-            <h3>On mobile device?</h3>
-            <a
-              href={reclaimUrl}
-              target="_blank"
-              rel="noreferrer"
-              className="App-link"
-            >
-              Click here to open on Reclaim Wallet App
-            </a>
-            <h3>On laptop/desktop?</h3>
-            <QRCode value={reclaimUrl} />
-            <p>or, Copy the link and send to your phone</p>
-            <CopyToClipboard text={reclaimUrl}>
-              <button onClick={notify} className="button">
-                Copy
-              </button>
-            </CopyToClipboard>
-          </>
-        ) : null}
-      </header>
-    </div>
+
+            {!reclaimUrl && (
+              <div>
+                <h2>DEMO</h2>
+                <h3>Prove that you are a real person</h3>
+                <p>[This demo uses HttpsProvider and CustomProvider]</p>
+
+                <div className="actions">
+                  <button
+                    className="button"
+                    disabled={!!reclaimUrl}
+                    onClick={proveIt}
+                  >
+                    Prove it!
+                  </button>
+                </div>
+              </div>
+            )}
+            {reclaimUrl && !proof && (
+              <div className="links">
+                {proofStatus && (
+                  <span>
+                    Status {proofStatus}... <CircularProgress />
+                  </span>
+                )}
+                <div>
+                  If you don't have our app installed, check the installation
+                  steps{" "}
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    className="App-link"
+                    href="https://questbook.gitbook.io/reclaim-protocol/installing-reclaim-wallet"
+                  >
+                    here
+                  </a>
+                </div>
+              </div>
+            )}
+            {reclaimUrl ? (
+              <>
+                <h3>On mobile device?</h3>
+                <a
+                  href={reclaimUrl}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="App-link"
+                >
+                  Click here to open on Reclaim Wallet App
+                </a>
+                <h3>On laptop/desktop?</h3>
+                <QRCode value={reclaimUrl} />
+                <p>or, Copy the link and send to your phone</p>
+                <CopyToClipboard text={reclaimUrl}>
+                  <button onClick={notify} className="button">
+                    Copy
+                  </button>
+                </CopyToClipboard>
+              </>
+            ) : null}
+          </header>
+        </div>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
